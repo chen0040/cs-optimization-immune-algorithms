5b0da839642c0143b3986841d1e8aca5da160bdd
xschen <xs0040@gmail.com>
2017-10-30 08:15:42 +0800

    binary solution representation

---
+++ cs-optimization-immune-algorithms/BinarySolver.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AIA
+{
+    public abstract class BinarySolver : BaseSolver<int>
+    {
+        public BinarySolver()
+        {
+
+        }
+
+        public delegate double CostEvaluationMethod(int[] x, object constraints);
+
+
+    }
+}
