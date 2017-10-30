4bd0107708f2064b30375975e8c6696c902766c6
xschen <xs0040@gmail.com>
2017-10-30 08:11:31 +0800

    source setup

---
+++ cs-optimization-immune-algorithms/ComponentModels/ISolution.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AIA.ProblemModels;
+
+namespace AIA.ComponentModels
+{
+    public interface ISolution
+    {
+        ISolution Clone();
+    }
+}
